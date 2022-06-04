14c18f3f86617de272d5a86340b8951dfbe26458
RexT1 <101406693+RexT1@users.noreply.github.com>
2022-06-04 22:32:32 +0700

    create weather

--- app/src/main/java/com/example/logreg/LoginActivity.kt
+++ app/src/main/java/com/example/logreg/LoginActivity.kt
@@ -3,19 +3,10 @@ package com.example.logreg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
@@ -73,4 +64,5 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
         }
     }
+
 }
--- app/src/main/java/com/example/logreg/MainActivity.kt
+++ app/src/main/java/com/example/logreg/MainActivity.kt
@@ -8,15 +8,21 @@ import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var mAuth: FirebaseAuth
     lateinit var btnLogout: Button
+    lateinit var btnWeather: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
+
+        btnWeather = findViewById(R.id.weather)
+        btnWeather.setOnClickListener {
+            val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+        }
 
         btnLogout = findViewById(R.id.logout)
         btnLogout.setOnClickListener {
